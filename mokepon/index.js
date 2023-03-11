b9f9d4ea50de76810df9a24a19e9c8a3b3725013
gerar10 <gerarburgos1987@gmail.com>
2023-03-11 10:24:56 -0300

    batalla

--- mokepon/index.js
+++ mokepon/index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const jugadores = [];
+
 class Jugador {
   constructor(id) {
     this.id = id;
@@ -15,12 +16,18 @@ class Jugador {
     this.x = x;
     this.y = y;
   }
+  asignarAtaques(ataques) {
+    this.ataques = ataques
+  }
 }
+
+
 class Mokepon {
   constructor(nombre) {
     this.nombre = nombre;
   }
 }
+
 app.get("/unirse", (req, res) => {
   const id = `${Math.random()}`;
   const jugador = new Jugador(id);
@@ -28,6 +35,7 @@ app.get("/unirse", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(id);
 });
+
 app.post("/mokepon/:jugadorId", (req, res) => {
   const jugadorId = req.params.jugadorId || "";
   const nombre = req.body.mokepon || "";
@@ -42,6 +50,7 @@ app.post("/mokepon/:jugadorId", (req, res) => {
   console.log(jugadorId);
   res.end();
 });
+
 app.post("/mokepon/:jugadorId/posicion", (req, res) => {
   const jugadorId = req.params.jugadorId || "";
   const x = req.body.x || 0;
@@ -55,6 +64,23 @@ app.post("/mokepon/:jugadorId/posicion", (req, res) => {
   const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id);
   res.send({ enemigos });
 });
+
+app.post("/mokepon/:jugadorId/ataques", (req, res) => {
+  const jugadorId = req.params.jugadorId || "";
+  const ataques = req.body.ataques || [];
+ 
+
+  const jugadorIndex = jugadores.findIndex(
+    (jugador) => jugadorId === jugador.id
+  );
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarAtaques(ataques);
+  }
+ 
+  res.end();
+});
+
 app.listen(8080, () => {
   console.log("Servidor funcionando");
 });
